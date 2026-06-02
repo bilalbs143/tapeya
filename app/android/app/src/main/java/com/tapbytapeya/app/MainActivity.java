380ce513bd5f09827e6ce84c86dcbc1f09c9189e
zain-here777 <zainii.cs@gmail.com>
2026-06-03 00:32:04 +0500

    android directory

---
+++ app/android/app/src/main/java/com/tapbytapeya/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tapbytapeya.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/android/app/src/main/java/com/tapeya/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tapeya.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
